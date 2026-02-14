ab6d26881d3f84b359c0a313b7dc87a9059538a8
praishaa <praishaa@gmail.com>
2026-02-14 18:45:17 +0530

    fixed

--- backend/config/firebase.js
+++ backend/config/firebase.js
@@ -1,5 +1,5 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("./serviceAccountKey.json");
+const serviceAccount = require("../firebaseKey.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
